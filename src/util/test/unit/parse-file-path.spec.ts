e8817b88b3417b8726438908a4cf3f3c277aee09
Rafael Uribe <Rafael.Uribe.2080@gmail.com>
2023-11-26 12:09:53 -0500

    Add jest and debug config

--- src/util/test/unit/parse-file-path.spec.ts
+++ src/util/test/unit/parse-file-path.spec.ts
@@ -1,12 +1,13 @@
-import { parseFilePath } from '../../parse-file-path';
+import { expect, it, describe } from "@jest/globals";
+import { parseFilePath } from "../../parse-file-path";
 
-describe('parseFilePath', () => {
-  it('should parse file path correctly', () => {
-    const filePath = '/path/to/your/file.txt';
+describe("parseFilePath", () => {
+  it("should parse file path correctly", () => {
+    const filePath = "/path/to/your/file.txt";
     const expectedFileInfo = {
-      directory: '\\path\\to\\your',
-      filename: 'file',
-      extension: 'txt',
+      directory: "\\path\\to\\your",
+      filename: "file",
+      extension: "txt",
     };
 
     const fileInfo = parseFilePath(filePath);
@@ -14,12 +15,12 @@ describe('parseFilePath', () => {
     expect(fileInfo).toEqual(expectedFileInfo);
   });
 
-  it('should handle Windows file paths', () => {
-    const filePath = 'C:\\path\\to\\your\\file.txt';
+  it("should handle Windows file paths", () => {
+    const filePath = "C:\\path\\to\\your\\file.txt";
     const expectedFileInfo = {
-      directory: 'C:\\path\\to\\your',
-      filename: 'file',
-      extension: 'txt',
+      directory: "C:\\path\\to\\your",
+      filename: "file",
+      extension: "txt",
     };
 
     const fileInfo = parseFilePath(filePath);
@@ -27,12 +28,12 @@ describe('parseFilePath', () => {
     expect(fileInfo).toEqual(expectedFileInfo);
   });
 
-  it('should handle file paths with spaces', () => {
-    const filePath = '/path/to/your/file with spaces.txt';
+  it("should handle file paths with spaces", () => {
+    const filePath = "/path/to/your/file with spaces.txt";
     const expectedFileInfo = {
-      directory: '\\path\\to\\your',
-      filename: 'file with spaces',
-      extension: 'txt',
+      directory: "\\path\\to\\your",
+      filename: "file with spaces",
+      extension: "txt",
     };
 
     const fileInfo = parseFilePath(filePath);
@@ -40,12 +41,12 @@ describe('parseFilePath', () => {
     expect(fileInfo).toEqual(expectedFileInfo);
   });
 
-  it('should handle file paths without extension', () => {
-    const filePath = '/path/to/your/file_without_extension';
+  it("should handle file paths without extension", () => {
+    const filePath = "/path/to/your/file_without_extension";
     const expectedFileInfo = {
-      directory: '\\path\\to\\your',
-      filename: 'file_without_extension',
-      extension: '',
+      directory: "\\path\\to\\your",
+      filename: "file_without_extension",
+      extension: "",
     };
 
     const fileInfo = parseFilePath(filePath);
